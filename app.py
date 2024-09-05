aa5a37c7c5ec6544e2a81b700377c70e97ff53a2
rohan space <alfredo.lm@hotmail.es>
2024-09-04 22:34:10 -0600

    Cambie la ruta relativa del Df

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv(
-    'D:/Tripleten/Proyecto6/vehicles_us.csv')  # leer los datos
+    '/proyecto6/vehicles_us.csv')  # leer los datos
 
 # crear una casilla de verificación
 build_histogram = st.checkbox('Histograma Vehiculos')
